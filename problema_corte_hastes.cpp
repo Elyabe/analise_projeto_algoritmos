d84c7d0825e14483a7aed150cb31e9b95d598620
elyabe <elyabe@outlook.com>
2018-10-04 19:48:26 -0300

    Implementação da memoização

--- problema_corte_hastes.cpp
+++ problema_corte_hastes.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 
 // Calcula e retorna maior lucro obtido pelo corte da haste
-// p : Vetor com preços de cada parte da haste preço/polegada
+// P : Vetor com preços de cada parte da haste preço/polegada
 // n : Comprimento da haste
 int cortar_haste( vector<int> P, int n )
 {
@@ -23,6 +23,41 @@ int cortar_haste( vector<int> P, int n )
 	return q;
 }
 
+// Função auxiliar : Caso memoizado
+// Calcula e retorna o maior lucro obtido pelo corte de uma haste
+// P : Vetor com preços de cada parte da haste (preço x polegada)
+// n : Comprimento da haste
+// R : Lucro memorizado para otimização
+int corte_haste_memoizado_aux( vector<int> P, int n, vector<int> R )
+{
+	int q;
+	if ( R[n] >= 0 ) 
+		return R[n];
+	else
+	{	
+		q = INF_NEG;
+		for ( int k = 1; k <= n; k++ )
+			q = max( q, P[k] + corte_haste_memoizado_aux( P, n-k, R ) );
+	}
+	
+	R[n] = q;
+
+	return q;
+}
+
+// Função geral
+// Calcula e retorna o maior lucro possível obtido no corte
+// P : Vetor com os preços por polegada
+// n : Comprimento da haste
+int cortar_haste_memoizado( vector<int> P, int n )
+{
+	vector<int> R(n+1, INF_NEG );
+	
+	// Caso haste com comprimento 0
+	R[0] = 0;
+	return corte_haste_memoizado_aux( P, n, R );
+}
+
 
 int main()
 {
@@ -34,6 +69,7 @@ int main()
 		cin>>P[k];
 
 	cout<<"Maior lucro possível: "<<cortar_haste( P, N )<<endl;
+	cout<<"Maior lucro possível (Memoizado): "<<cortar_haste_memoizado( P, N )<<endl;
 	
 	return 0;
 }
